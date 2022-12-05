0f8ddbf066287c80f4b6ab61c671e3ed703d1dd6
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2022-12-05 20:30:11 +0000

    updated plugins.bash

--- bash/plugins.bash
+++ bash/plugins.bash
@@ -2,3 +2,42 @@
 if [[ "$(tput colors)" == "256" ]]; then
     eval "$(dircolors ~/.shell/plugins/dircolors-solarized/dircolors.256dark)"
 fi
+
+# Load starship prompt if starship is installed
+if  [ -x /usr/bin/starship ]; then
+    __main() {
+        local major="${BASH_VERSINFO[0]}"
+        local minor="${BASH_VERSINFO[1]}"
+
+        if ((major > 4)) || { ((major == 4)) && ((minor >= 1)); }; then
+            source <("/usr/bin/starship" init bash --print-full-init)
+        else
+            source /dev/stdin <<<"$("/usr/bin/starship" init bash --print-full-init)"
+        fi
+    }
+    __main
+    unset -f __main
+fi
+
+
+# Advanced command-not-found hook
+source /usr/share/doc/find-the-command/ftc.bash
+
+
+# Fastfetch
+if [ -t 0 ]; then
+    if type -p "fastfetch" > /dev/null; then
+        fastfetch
+    else
+        echo "Warning: fastfetch was called, but it's not installed."
+    fi
+fi
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+### Bashhub.com Installation.
+### This Should be at the EOF. https://bashhub.com/docs
+if [ -f ~/.bashhub/bashhub.sh ]; then
+    source ~/.bashhub/bashhub.sh
+fi
+
