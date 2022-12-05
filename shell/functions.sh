bfe985f9e16b11b7d453d8d75e681086ba2ad974
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2022-12-05 20:05:51 +0000

    added transmissionrc 2 torrent and added bash functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -28,3 +28,138 @@ there="$HOME/.shell.here"
 there() {
     cd "$(readlink "${there}")"
 }
+
+# Load starship prompt if starship is installed
+if  [ -x /usr/bin/starship ]; then
+    __main() {
+        local major="${BASH_VERSINFO[0]}"
+        local minor="${BASH_VERSINFO[1]}"
+
+        if ((major > 4)) || { ((major == 4)) && ((minor >= 1)); }; then
+            source <("/usr/bin/starship" init bash --print-full-init)
+        else
+            source /dev/stdin <<<"$("/usr/bin/starship" init bash --print-full-init)"
+        fi
+    }
+    __main
+    unset -f __main
+fi
+
+# Advanced command-not-found hook
+source /usr/share/doc/find-the-command/ftc.bash
+
+# Fastfetch
+if [ -t 0 ]; then
+    if type -p "fastfetch" > /dev/null; then
+        fastfetch
+    else
+        echo "Warning: fastfetch was called, but it's not installed."
+    fi
+fi
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+### Bashhub.com Installation.
+### This Should be at the EOF. https://bashhub.com/docs
+if [ -f ~/.bashhub/bashhub.sh ]; then
+    source ~/.bashhub/bashhub.sh
+fi
+
+## Git Aliases
+gcom() {
+    git add .
+    git commit -m "$1"
+    }
+lazyg() {
+    git add .
+    git commit -m "$1"
+    git push
+}
+
+
+# Fetch pull request
+fpr() {
+    if ! git rev-parse --git-dir > /dev/null 2>&1; then
+        echo "error: fpr must be executed from within a git repository"
+        return 1
+    fi
+    (
+        cdgr
+        if [ "$#" -eq 1 ]; then
+            local repo="${PWD##*/}"
+            local user="${1%%:*}"
+            local branch="${1#*:}"
+        elif [ "$#" -eq 2 ]; then
+            local repo="${PWD##*/}"
+            local user="${1}"
+            local branch="${2}"
+        elif [ "$#" -eq 3 ]; then
+            local repo="${1}"
+            local user="${2}"
+            local branch="${3}"
+        else
+            echo "Usage: fpr [repo] username branch"
+            return 1
+        fi
+
+        git fetch "git@github.com:${user}/${repo}" "${branch}:${user}/${branch}"
+    )
+}
+
+
+
+# Go up [n] directories
+up()
+{
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number"
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive"
+    else
+        for ((i=0; i<${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
+}
+
+# Create a directory and cd into it
+mcd() {
+    mkdir "${1}" && cd "${1}"
+}
+
+# Jump to directory containing file
+jump() {
+    cd "$(dirname ${1})"
+}
+
+
+# Update dotfiles
+dfu() {
+    (
+        cd ~/.dotfiles && git pull --ff-only && ./install -q
+    )
+}
+
+# Use pip without requiring virtualenv
+syspip() {
+    PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
+
+syspip2() {
+    PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
+}
+
+syspip3() {
+    PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
+}
+
+
